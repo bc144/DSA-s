e6813b0798652f7f85022a917fd634329ae4457f
Bruno Contreras <bc644644@gmail.com>
2025-07-02 12:06:37 -0600

    Brute Force Solution : Contains Duplicate

--- Neetcode Roadmap/Arrays and Hashing /containsDuplicate.py
+++ Neetcode Roadmap/Arrays and Hashing /containsDuplicate.py
@@ -17,3 +17,17 @@ Output: false
 
 '''
 
+from typing import List
+
+#Brute force solution , Time complexity O(n^2), Space Complexity: O(1)
+
+class BruteForce:
+    def hasDuplicate(self,nums:List[int])->bool:
+        for i in range(len(nums)):  #Iterate over each element of the list
+            for j in range (i+1,len(nums)): # Compare with every element ahead of it
+                if nums[i] == nums[j]:  # Check for duplication
+                    return True
+    
+        return False  #No duplicates
+           
+        
