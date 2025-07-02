9ecd4dcdbb525dfa462073a98bb711e1b518a88a
Bruno Contreras <bc644644@gmail.com>
2025-07-02 12:53:01 -0600

    Sorting solution with test cases and most optimal solution

--- Neetcode Roadmap/Arrays and Hashing /containsDuplicate.py
+++ Neetcode Roadmap/Arrays and Hashing /containsDuplicate.py
@@ -31,12 +31,33 @@ class BruteForce:
         return False  #No duplicates
     
 
+#Sorting solution, Time complexity O(n log n), Space Complexity: O(1)
 
-           
+class SortingList:
+    def hasDuplicate(self,nums:List[int])->bool:
+        nums.sort()
+        for i in range(len(nums)-1): #Iterate over each element of the sorted list
+            if nums[i] == nums[i+1]: #Compare an element of the list with the next element of the list to see if they are equal
+                return True
+        return False # No duplicates
+    
+#Hashset solution, most optimal solution, Time complexity O(n), Space Complexity: O(n), space complexity in sacrificed due to the possibility of hashset being the same length as the list.
+
+class OptimalSolution:
+    def hasDuplicate(self,nums:List[int])->bool:
+        hashset = set() # Turn the list into a set
+
+        for n in nums: #Iterate over each element in the list 
+            if n in hashset: #Check if the element exist on the list
+                return True
+            hashset.add(n) #Adds the element if it is not in the set
+        return False #No duplicates
+
         
 # Entry point for running test cases
 if __name__ == "__main__":
     solution1 = BruteForce()
+    solution2 = SortingList()
 
     # Test cases
     test_cases = [
@@ -50,5 +71,7 @@ if __name__ == "__main__":
 
     # Running and printing results
     for i, (input_list, expected) in enumerate(test_cases):
-        result = solution1.hasDuplicate(input_list)
-        print(f"Test Case {i + 1}: Input: {input_list} | Expected: {expected} | Result: {result} | {'✅' if result == expected else '❌'}")
+        result1 = solution1.hasDuplicate(input_list)
+        result2 = solution2.hasDuplicate(input_list)
+        print(f"Test Case {i + 1}: Input: {input_list} | Expected: {expected} | Result: {result1} | {'✅' if result1 == expected else '❌'}")
+        print(f"Test Case {i + 1}: Input: {input_list} | Expected: {expected} | Result: {result2} | {'✅' if result2 == expected else '❌'}")
